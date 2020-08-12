08118ae327bfa6c1f1fa74261f71058835393643
ics-konno <konno@ics-web.jp>
2020-08-12 20:01:43 +0900

    ルーター、リンクの追加

--- vuetify-dashboard/src/App.vue
+++ vuetify-dashboard/src/App.vue
@@ -3,58 +3,33 @@
     <v-app-bar app color="primary" dark>
       <v-toolbar-title>Vuetify Dashboard</v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn text rounded>Home</v-btn>
-      <v-btn text rounded>Login</v-btn>
+      <v-btn
+        v-for="link in links"
+        :key="`header-link-${link.label}`"
+        text
+        rounded
+        :to="link.path"
+        >{{ link.label }}</v-btn
+      >
     </v-app-bar>
     <v-content>
-      <!--  Login Module  -->
-      <v-card width="400px" class="mt-5 mx-auto">
-        <v-card-title class="pb-0">
-          <h1 class="display-1">Login</h1>
-        </v-card-title>
-        <v-card-text>
-          <v-form>
-            <v-text-field label="Username" prepend-icon="mdi-account-circle" />
-            <v-text-field
-              :type="showPassword ? `text` : `password`"
-              label="Password"
-              prepend-icon="mdi-lock"
-              :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-              @click:append="showPassword = !showPassword"
-            />
-          </v-form>
-        </v-card-text>
-        <v-divider></v-divider>
-        <v-card-actions>
-          <v-btn color="success">Register</v-btn>
-          <v-spacer></v-spacer>
-          <v-btn color="info">Login</v-btn>
-        </v-card-actions>
-      </v-card>
+      <router-view></router-view>
     </v-content>
 
-    <v-footer
-        color="primary lighten-1"
-        padless
-    >
-      <v-row
-          justify="center"
-          no-gutters
-      >
+    <v-footer color="primary lighten-1" padless>
+      <v-row justify="center" no-gutters>
         <v-btn
-            v-for="link in links"
-            :key="link"
-            color="white"
-            text
-            rounded
-            class="my-2"
+          v-for="link in links"
+          :key="`footer-link-${link.label}`"
+          color="white"
+          text
+          rounded
+          class="my-2"
+          :to="link.path"
         >
-          {{ link }}
+          {{ link.label }}
         </v-btn>
-        <v-col
-            class="primary lighten-2 py-4 text-center white--text"
-            cols="12"
-        >
+        <v-col class="primary lighten-2 py-4 text-center white--text" cols="12">
           {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
         </v-col>
       </v-row>
@@ -70,7 +45,10 @@ export default {
 
   data: () => ({
     showPassword: false,
-    links: ["HOME", "LOGIN"]
+    links: [
+      { label: "HOME", path: "/" },
+      { label: "LOGIN", path: "/login" },
+    ],
   }),
 };
 </script>
